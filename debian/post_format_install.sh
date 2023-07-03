111fa1ea8dbec5f8a13f6fa26bdac27b05283fcc
Arun Rana <arun0808rana@gmail.com>
2023-07-03 10:47:05 +0530

    thefuck .bashrc integration for aliasing

--- debian/post_format_install.sh
+++ debian/post_format_install.sh
@@ -2,30 +2,34 @@
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install curl -y
-sudo apt install tldr -y
-sudo apt install thefuck -y
-sudo apt install xclip -y
-sudo apt install fzf -y
-sudo apt install vlc -y
-sudo apt install gnome-tweaks -y
+sudo apt install -y nvidia-driver firmware-misc-nonfree
+sudo apt install -y curl
+sudo apt install -y tldr
+sudo apt install -y thefuck
+sudo apt install -y xclip
+sudo apt install -y fzf
+sudo apt install -y vlc
+sudo apt install -y gnome-tweaks
+sudo apt install -y lsd
+sudo apt install -y bat
 mkdir ~/.themes
 mkdir ~/.icons
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update -y
-sudo apt install brave-browser -y
-sudo apt install git -y
+sudo apt install -y brave-browser
+sudo apt install -y git
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
-sudo apt update -y && sudo apt install codium -y
+sudo apt update -y && sudo apt install -y codium
 
 curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - &&\
 apt-get install -y nodejs
 
+echo -e "\n# this needs to be at the end\neval \$(thefuck --alias fuck)" >> ~/.bashrc
 
 
 
