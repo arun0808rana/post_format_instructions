6340fa6fd1464d166da1934b8848639a7b2c8754
Arun Rana <arun0808rana@gmail.com>
2023-07-03 16:59:51 +0530

    Update post_format_install.sh

--- debian/post_format_install.sh
+++ debian/post_format_install.sh
@@ -12,14 +12,19 @@ sudo apt install -y vlc
 sudo apt install -y gnome-tweaks
 sudo apt install -y lsd
 sudo apt install -y bat
-sudo apt install -y gnome-screenshot
-mkdir ~/.themes
-mkdir ~/.icons
+sudo apt install -y git
+
+echo '--------------Making .themes and .icons directories----------------'
+mkdir "/home/$USER/.themes"
+mkdir "/home/$USER/.icons"
+
+echo '-------------Installing Brave Browser-----------------'
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update -y
 sudo apt install -y brave-browser
-sudo apt install -y git
+
+echo '-------------Installing Codium-----------------'
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
@@ -27,15 +32,23 @@ echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https:/
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 sudo apt update -y && sudo apt install -y codium
 
+echo '-------------Installing Nodejs-----------------'
 curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - &&\
 apt-get install -y nodejs
+
+echo '-------------Installing Yarn-----------------'
 sudo npm install --global yarn
 
-echo -e "\n# this needs to be at the end\neval \$(thefuck --alias fuck)" >> ~/.bashrc
+echo '----------theFuck Integration in .bashrc file---------------'
+echo -e "\n# this needs to be at the end\neval \$(thefuck --alias fuck)" >> "/home/$USER/.bashrc"
 
-mkdir -p ~/.config && touch ~/.config/starship.toml
+echo '------------Making .config dir in root and creating starship.toml inside it--------------'
+mkdir -p "/home/$USER/.config"
+touch "/home/$USER/.config/starship.toml"
 
-cat <<EOF > ~/.config/starship.toml
+echo '----------------Cating starship configuration in starship.toml file----------------'
+
+cat <<EOF > "/home/$USER/.config/starship.toml"
 # Get editor completions based on the config schema
 "\$schema" = 'https://starship.rs/config-schema.json'
 
@@ -51,26 +64,12 @@ error_symbol = '[âœ—](bold red)' # The 'error_symbol' segment is being set to 'â
 style = '#3e73d0'
 EOF
 
-echo -e "\n\n# this needs to be at the end\neval \"\$(starship init bash)\"" >> ~/.bashrc
+echo '---------starship integration in .bashrc----------------'
+
+echo -e "\n\n# this needs to be at the end\neval \"\$(starship init bash)\"" >> "/home/$USER/.bashrc"
+
+echo '-------------Installing starship-----------------'
 
 curl -sS https://starship.rs/install.sh | sh
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+source "/home/$USER/.bashrc"
