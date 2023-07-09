87a4be5345979e5fcbb8575ecfbb3b2a2a8f3ea9
Arun Rana <arun0808rana@gmail.com>
2023-07-09 15:14:10 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# copy .bashrc into dev_profile/configs
+cp /home/$USER/.bashrc /home/$USER/dev_profile/configs/.bashrc
+
+# copy .bash_aliases into dev_profile/configs
+cp /home/$USER/.bash_aliases /home/$USER/dev_profile/configs/.bash_aliases
+
 # Source folder to be backed up
 SOURCE_FOLDER="/home/soydev/dev_profile"
 
